153b1eda24e583547a0eeae85d74ff73454c363b
Irving <irvingtx@live.cojm>
2024-01-08 18:07:24 -0600

    Added visualiztion x distance y channels z defelections

--- main.py
+++ main.py
@@ -18,20 +18,27 @@ z_data=np.genfromtxt(Data,delimiter=",",dtype=float)
 
 #Startup coordinates for visual x - footage and z - deflections and y will be the channel numbers
 xx,yy=np.meshgrid(x_data,y_data)
-print(x_data[0])
-x_new=np.arange(x_data[0],x_data[len(x_data)-1],0.5)
+
+x_new=np.arange(x_data[0],x_data[len(x_data)-1],0.01)
+
 #calculate cubicv spline
 #x_new=np.arange(x_data)
 
 #for iterator in range(len(z_data)):
-#CALCULATE SPOLINE FOR DATA SET
+
+#CALCULATE SPLINE FOR DATA SET
 for iterator in range(len(z_data)):
-    cubic_iterator=CubicSpline(x_data,z_data[0],bc_type='natural')(x_new)
-    cubic_spline=np.stack(cubic_iterator)
+    cubic_iterator=CubicSpline(x_data,z_data[iterator],bc_type='natural')(x_new)
+    if(iterator==0):
+        cubic_final=cubic_iterator
+    else:
+        cubic_final=np.vstack((cubic_final,cubic_iterator))
 
-plt.plot(x_new,cubic,'o', label=f'Test Profile')
-
+#mesh new coordinates for calculated spline and new x values
+x_spline,y_spline=np.meshgrid(x_new,y_data)
+print(cubic_final)
 fig=plt.figure()
 ax=fig.add_subplot(111,projection="3d")
-ax.plot_surface(xx,yy,z_data,cmap="plasma", linewidth=0, antialiased=False, alpha=0.5)
+
+ax.plot_surface(x_spline,y_spline,cubic_final,cmap="plasma", linewidth=0, antialiased=False, alpha=0.5)
 plt.show()
